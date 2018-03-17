f302225c2a711a6ac3c6c02dc3606e737a270153
nobii <fujisawa-shin@kayac.com>
2018-03-17 11:49:00 +0900

    書き出し先を変える

--- config/index.js
+++ config/index.js
@@ -4,6 +4,8 @@
 
 const path = require('path')
 
+const DEST = '../public';
+
 module.exports = {
   dev: {
 
@@ -45,10 +47,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, `${DEST}/index.html`),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, DEST),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
